8721cd06d92defaf9638fd948fc311677ab85e77
AmirHosseinP2002 <amirhossein.poursaeed1@gmail.com>
2025-08-01 13:24:17 +0330

    Initial settings for project.

---
+++ src/__init__.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from src.db.main import sessionmanager
+from src.config import Config
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    if sessionmanager._engine is not None:
+        await sessionmanager.close()
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    title=Config.PROJECT_NAME,
+    version=Config.VERSION,
+    description='A REST API for Book service with reviews.'
+)
